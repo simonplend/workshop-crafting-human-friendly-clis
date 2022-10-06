4d50b8739f9fb5e391d2e8e41c2acf310b6e3da0
Kyriakos Markakis <11089475+marquicodes@users.noreply.github.com>
2022-10-06 09:17:48 +0100

    docs: Correct typo and punctuation (#14)
    Co-authored-by: Kyriakos Markakis <>

--- .template/lib/02-interactive-input.js
+++ .template/lib/02-interactive-input.js
@@ -2,13 +2,13 @@
   == Exercise: Interactive input ==
 
   - If the CLI tool is run without command line arguments,
-    it should un in interactive input mode.
+    it should run in interactive input mode.
   - Greet the user
   - Prompt for a note
   - For each new line, continue to prompt for a note
   - If the user returns an empty line, confirm that they are finished
-    - If not keep collecting notes
-    - If they are prompt to ask if they'd like these notes to be private
+    - If not, keep collecting notes
+    - If they are, prompt to ask if they'd like these notes to be private
   - Return an object with the following shape:
 
   ```
